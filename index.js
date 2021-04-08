e9c162dd705e59c26803b8457c8d150faeda0a8b
Nija <nija.perry21@gmail.com>
2021-04-08 13:40:23 -0400

    commit tree d74ebd8ab3952588b5a24e1e6bfb0044ff49c2e3

--- index.js
+++ index.js
@@ -8,21 +8,51 @@ function showMeTheMoney() {
     .then(res => res.json())
     .then(data => {
       console.log(data)
-      //how to sort data by type?
-      // data.filter("type")?
-      //How to grab id=currency-select?
-      // const cryptoList = document.getElementsByName("crypto")
-      const cryptoList = document.querySelector("value1")
-      // if (cryptoList == "Crypto")
-      console.log(cryptoList)
-      const fiatList = document.getElementsByName("fiat")
-      console.log(fiatList)
 
-      const commoditiesList = document.getElementsByName("commodities")
-      console.log(commoditiesList)
+      //Send JSON data into a callback function
+      function cryptoList(data) {
+        data.filter()
+        // for (const element of data) {
+
+        //Find the container where we attach everything to
+        const currencyUl = document.querySelector('#container');
+
+        //Create all necessary elements
+        const cryptoList = document.createElement('li');
+        const fiatList = document.createElement('li');
+        const commodotiesList = document.createElement('li');
+
+        //Add appropriate classes and ids. Grab data and insert if needed.
+        cryptoList.className = "Crypto"
+        fiatList.classList = "Fiat"
+        commodotiesList.classList = "Commodities"
+
+        cryptoList.dataset.id = element.id
+
+
+
+          //Grab data and insert it into created elements
+          .innerHTML
+
+        //Append everything to main container
+
+      }
+
 
     })
-  return showMeTheMoney;
+
+  // const cryptoList = document.getElementsByName("crypto")
+  // // if (cryptoList == "Crypto")
+  // console.log(cryptoList)
+
+  // const fiatList = document.getElementsByName("fiat")
+  // console.log(fiatList)
+
+  // const commoditiesList = document.getElementsByName("commodities")
+  // console.log(commoditiesList)
+
+  // })
+  // showMeTheMoney();
 }
 
 
