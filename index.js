0e1d25a26b3b42a9b9f9f34d4f6e5864f1351168
Nija <nija.perry21@gmail.com>
2021-04-09 11:15:19 -0400

    project1

--- index.js
+++ index.js
@@ -4,60 +4,41 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 function showMeTheMoney() {
+  let chosenCurrency = event.target.value
   fetch(BASE_URL)
     .then(res => res.json())
     .then(data => {
       const currencies = Object.values(data.rates);
-      let chosenCurrency = ""
-      console.log(currencies)
-      currencies.filter(currency => currency.type === chosenCurrency)
+      const filteredCurrencies = currencies.filter(currency => currency.type === chosenCurrency)
+      console.log(filteredCurrencies)
 
-      //currencies.filter(currency => currency.type === "crypto")
 
-      //Send JSON data into a callback function
-      // seperate by data type
-      // data.filter()
-      //filter is confusing.  it is not a function so why the parentheses? do I place it in the currency function param?
 
-      // for (const element of data) {
 
       //Find the container where we attach everything to
-      const currencyUl = document.querySelector('#container');
+      // const currencyUl = document.querySelector('#container');
 
       //Create all necessary elements
-      const cryptoList = document.createElement('li');
-      const fiatList = document.createElement('li');
-      const commodotiesList = document.createElement('li');
+      // const cryptoList = document.createElement('li');
+      // const fiatList = document.createElement('li');
+      // const commodotiesList = document.createElement('li');
 
       //Add appropriate classes and ids. Grab data and insert if needed.
-      cryptoList.className = "Crypto"
-      fiatList.classList = "Fiat"
-      commodotiesList.classList = "Commodities"
+      // cryptoList.className = "Crypto"
+      // fiatList.classList = "Fiat"
+      // commodotiesList.classList = "Commodities"
       //how to grab data?
 
-      //whatgoeshere?.dataset.id = element.id
+
 
 
 
       //Grab data and insert it into created elements
       //cryptoList.innerHTML? 
 
-      //Append everything to main containe
+      //Append everything to main container
 
     })
-
-  // const cryptoList = document.getElementsByName("crypto")
-  // // if (cryptoList == "Crypto")
-  // console.log(cryptoList)
-
-  // const fiatList = document.getElementsByName("fiat")
-  // console.log(fiatList)
-
-  // const commoditiesList = document.getElementsByName("commodities")
-  // console.log(commoditiesList)
-
-  // })
-  // showMeTheMoney();
 }
 
 
