12f018342f335c4ac365afc28f07a91b0a4f364d
Nija <nija.perry21@gmail.com>
2021-04-09 13:57:34 -0400

    project1

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 const BASE_URL = "https://api.coingecko.com/api/v3/exchange_rates"
 document.addEventListener("DOMContentLoaded", () => {
-  document.addEventListener("change", showMeTheMoney)
+  // document.addEventListener("change", showMeTheMoney)
+  document.querySelector("#currency-select").addEventListener("change", showMeTheMoney)
 })
 
 function showMeTheMoney() {
   let chosenCurrency = event.target.value
+  console.log(chosenCurrency)
   fetch(BASE_URL)
     .then(res => res.json())
     .then(data => {
       const currencies = Object.values(data.rates);
       const filteredCurrencies = currencies.filter(currency => currency.type === chosenCurrency)
+      renderCurrencyList(filteredCurrencies)
+      addListenersToLi()
       console.log(filteredCurrencies)
-
-      //iterate through currency and place in proper container
-      array.forEach(chosenCurrency => {
-
-
-      });
-
-      //Find the container where we attach everything to
-      const currencyUl = document.querySelector('#container');
-
-      //Create all necessary elements
-      const cryptoList = document.createElement('li');
-      const fiatList = document.createElement('li');
-      const commodotiesList = document.createElement('li');
-
-      //Add appropriate classes and ids. Grab data and insert if needed.
-      // cryptoList.className = "Crypto"
-      // fiatList.classList = "Fiat"
-      // commodotiesList.classList = "Commodities"
-
-
-      //Append everything to main container
-
     })
 }
+//create helper function
+function renderCurrencyList(list) {
+  const currencyList = document.querySelector(".currencyList")
+  currencyList.innerHTML = ""
+  for (let i = 0; i < list.length; i++) {
+    const listItem = document.createElement('li');
+    listItem.innerText = list[i].name
+    listItem.className = "investment-options"
+    listItem.setAttribute("data-value", list[i].value)
+    currencyList.appendChild(listItem)
+
+
+  }
+}
+
+//second helper function to add event listeners
+function addListenersToLi() {
+  const listItems = document.querySelectorAll(".investment-options")
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener("click", function (event) {
+      alert("value: " + event.target.dataset.value)
+    })
+  }
+}
+
+//function to show currency value
+function showCurrencyValue() {
+
+
+}
+
 
 
