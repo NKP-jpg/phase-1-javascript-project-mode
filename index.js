19eae7e50f765309ab436231c9d198b2d7163435
Nija <nija.perry21@gmail.com>
2021-04-14 10:50:54 -0400

    project1

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#currency-select").addEventListener("change", showMeTheMoney)
 })
 
-
 function showMeTheMoney() {
   let chosenCurrency = event.target.value
   console.log(chosenCurrency)
@@ -13,7 +12,6 @@ function showMeTheMoney() {
       const filteredCurrencies = currencies.filter(currency => currency.type === chosenCurrency)
       renderCurrencyList(filteredCurrencies)
       addListenersToLi()
-      // showCurrencyValue()
       console.log(filteredCurrencies)
     })
 }
@@ -29,7 +27,6 @@ function renderCurrencyList(list) {
     currencyList.appendChild(listItem)
   }
 }
-
 //second helper function to add event listeners
 function addListenersToLi() {
   const listItems = document.querySelectorAll(".investment-options")
@@ -37,15 +34,12 @@ function addListenersToLi() {
     listItems[i].addEventListener("click", showCurrencyValue)
   }
 }
-
 //third helper function to display currency value
 function showCurrencyValue() {
   let currencyList = document.querySelector(".currencyList")
   currencyList.innerHTML = ""
   let currencyValue = document.createElement('li')
-  currencyValue.innerText = event.target.innerText + "-  Value: " + event.target.dataset.value
-  // currencyValue.className = "value"
-  // currencyValue.setAttribute("value", event.target.dataset.value)
+  currencyValue.innerText = event.target.innerText + " Value: " + event.target.dataset.value
   currencyList.appendChild(currencyValue)
 }
 
